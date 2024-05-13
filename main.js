892d5d60841cb4caa9a49cd3c65beb92d0a5db72
sagormohammedyousuf <smyousuf7999@gmail.com>
2024-05-13 08:31:47 +0600

    update-4

--- main.js
+++ main.js
@@ -23,7 +23,13 @@ const workLink = document.getElementById('work-link');
 const healthLink = document.getElementById('health-link');
 const exerciseLink = document.getElementById('exercise-link');
 
+
+const allLink = document.querySelectorAll('ul li a');
+
 sleepLink.addEventListener('click', () => {
+
+   
+
     sleep.style.display = 'flex';
     health.style.display = 'none';
     wakeUp.style.display = 'none';
@@ -31,6 +37,18 @@ sleepLink.addEventListener('click', () => {
     powerNap.style.display = 'none';
     work.style.display = 'none';
     home.style.display = 'none';
+
+    
+
+    sleepLink.classList.add('active');
+    wakeLink.classList.remove('active');
+    powerLink.classList.remove('active');
+    healthLink.classList.remove('active');
+    exerciseLink.classList.remove('active');
+    wakeLink.classList.remove('active');
+
+
+   
 });
 
 wakeLink.addEventListener('click', () => {
@@ -41,6 +59,16 @@ wakeLink.addEventListener('click', () => {
     powerNap.style.display = 'none';
     work.style.display = 'none';
     home.style.display = 'none';
+
+    wakeLink.classList.add('active');
+
+    
+    sleepLink.classList.remove('active');
+    
+    powerLink.classList.remove('active');
+    healthLink.classList.remove('active');
+    exerciseLink.classList.remove('active');
+    workLink.classList.remove('active');
 });
 
 powerLink.addEventListener('click', () => {
@@ -51,6 +79,16 @@ powerLink.addEventListener('click', () => {
     exercise.style.display = 'none';
     work.style.display = 'none';
     home.style.display = 'none';
+    powerLink.classList.add('active');
+
+    
+    sleepLink.classList.remove('active');
+    wakeLink.classList.remove('active');
+    // powerLink.classList.remove('active');
+    healthLink.classList.remove('active');
+    exerciseLink.classList.remove('active');
+    workLink.classList.remove('active');
+
 });
 
 workLink.addEventListener('click', () => {
@@ -61,6 +99,19 @@ workLink.addEventListener('click', () => {
     exercise.style.display = 'none';
     powerNap.style.display = 'none';
     home.style.display = 'none';
+
+
+
+    workLink.classList.add('active');
+
+
+    
+    sleepLink.classList.remove('active');
+    wakeLink.classList.remove('active');
+    powerLink.classList.remove('active');
+    healthLink.classList.remove('active');
+    exerciseLink.classList.remove('active');
+    
 });
 
 healthLink.addEventListener('click', () => {
@@ -71,6 +122,18 @@ healthLink.addEventListener('click', () => {
     powerNap.style.display = 'none';
     work.style.display = 'none';
     home.style.display = 'none';
+    // healthLink.classList.add('active');
+
+
+    
+    sleepLink.classList.remove('active');
+    wakeLink.classList.remove('active');
+    powerLink.classList.remove('active');
+    healthLink.classList.add('active');
+    exerciseLink.classList.remove('active');
+    workLink.classList.remove('active');
+
+    
 });
 
 exerciseLink.addEventListener('click', () => {
@@ -82,5 +145,18 @@ exerciseLink.addEventListener('click', () => {
     work.style.display = 'none';
     home.style.display = 'none';
 
- 
+
+
+    
+    sleepLink.classList.remove('active');
+    wakeLink.classList.remove('active');
+    powerLink.classList.remove('active');
+    healthLink.classList.remove('active');
+    exerciseLink.classList.add('active');
+    workLink.classList.remove('active');
+
+    
 });
+
+
+
